378334183c3e2139becf5b82a18c1d35994ee0ee
STZhang <zhangst11@mails.tsinghua.edu.cn>
2014-07-11 19:03:49 +0800

    edit image source

--- FrontEnd/HW4/js/slideshow.js
+++ FrontEnd/HW4/js/slideshow.js
@@ -10,10 +10,13 @@ $(document).ready(function(){
 		dataType: "json",
 		//请求成功完成后要执行的方法
 		success: function(data) {
-			//使用$.each方法遍历返回的数据date,插入到id为#result中
+			//使用$.each方法遍历返回的数据
 			$.each(data, function(i, item) {
 				i = i + 1;
 				$(".slideshow").append($("<li><span>"+ i +"</span><div><h5>" + item.content + "</h5><h3>" + item.title + "</h3></div></li>"));
+				$(".slideshow ul li").each(function(index){
+					$(this).css("background-image", "url("+ imageSrc +")");
+				});
 			})
 		}
 	});
