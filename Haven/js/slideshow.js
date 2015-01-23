9ef70741cfd9f210e2a3959be399d3fdef58e4f2
STZhang <zhangst11@mails.tsinghua.edu.cn>
2015-01-23 10:04:39 +0800

    add new page

--- Haven/js/slideshow.js
+++ Haven/js/slideshow.js
@@ -1,6 +1,14 @@
 
 $(document).ready(function(){
 	$("#enter").click(function(){
-		 window.location.href = "http://hadonghoon.cn/bbs/forum.php"; 
+		 //window.location.href = "http://hadonghoon.cn/bbs/forum.php";
+		 window.location.href = "./index2.html";
 	});
+	$("#enter").each (function (dom)
+    {
+        dom.ontouchstart = function()
+        {
+            window.location.href = "./index2.html";
+        }
+    });
 });
